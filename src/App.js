40833af195bcf0a421c1cb5357e5b76c595b9de8
Rohith M <rohith.m94@gmail.com>
2020-10-04 18:11:42 +0530

    created  react custpm accordion component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Accordion from "./components/Accordion/Accordion";
+import Search from "./components/Search/Search";
+
+const items = [
+  {
+    title: "What is React .?",
+    content: "React is Front end JavaScript frameWork",
+  },
+  {
+    title: "What is VueJS .?",
+    content: "VueJS is Front end JavaScript frameWork",
+  },
+  {
+    title: "What is React ..?",
+    content: "React is Front end JavaScript frameWork",
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <Accordion items={items} /> */}
+      <Search />
     </div>
   );
 }
